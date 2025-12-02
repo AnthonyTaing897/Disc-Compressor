4cc24b8402c6db0f4bb85577707242e995e078f2
Loafcifer <taing897@gmail.com>
2025-12-02 21:07:49 +1100

    Update Compress_Video_Function.py
    Constructed a operating compression function that can compile videos into an approximate size.

--- Discord_Bot/cmds/func/Compress_Video_Function.py
+++ Discord_Bot/cmds/func/Compress_Video_Function.py
@@ -3,9 +3,12 @@
 import ffmpeg
 from pathlib import Path
 
-def compressVid (video_file:Path,processed_drct:Path,filename:Path, target_file_size: int) -> Path:
-    #file size (mbs)
-    file_size = target_file_size * 1000
+def compressVid (video_file:Path,processed_drct:Path,filename:Path, target_file_size: int, compressAudio = False) -> Path:
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+
+    #file size (in bytes)
+    target_size = target_file_size * 1024 * 1024
     
     probed_vid = ffmpeg.probe(str(video_file))
 
@@ -14,22 +17,46 @@ def compressVid (video_file:Path,processed_drct:Path,filename:Path, target_file_
 
     #Audio bitrate (bps)
     audio_bitrate = float(next((s for s in probed_vid['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
-    
-    print(audio_bitrate)
 
-    #Previous code
-    '''
-    process_path = f"{processed_drct}/{filename}_compressed.mp4"
-    print(process_path)
+    #Calculate target video bitrate (bps) 
+    target_bitrate = ((target_size * 8) / (1.073741824 * vid_duration)) #1.073741824 to convert bytes to bits
+
+    #Calculate target audio bitrate (bps)
+    if 10 * audio_bitrate > target_bitrate:
+        audio_bitrate = target_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+
+    # Video bitrate (bps)
+    video_bitrate = target_bitrate - audio_bitrate
 
     video = ffmpeg.input(video_file)
-    audio = video.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
-    video = video.video.hflip()
-    output = ffmpeg.output(audio, video, process_path)
     
-    ffmpeg.run(output, overwrite_output=True)
-    return process_path'''
+    compressed_video_filepath = f"{processed_drct}/{filename}_compressed.mp4"
+    # Output compressed video
+
+    if compressAudio:
+        ffmpeg.output(video, compressed_video_filepath,
+                      vcodec = 'libx265',
+                      video_bitrate = int(video_bitrate),
+                      audio_bitrate = int(audio_bitrate),
+                      format='mp4').run(overwrite_output=True)
+                    
+    else:
+        ffmpeg.output(video, compressed_video_filepath,
+                      vcodec = 'libx265',
+                      video_bitrate = int(video_bitrate),
+                      format='mp4').run(overwrite_output=True)
+
+    #Get final file size
+    final_size = Path(compressed_video_filepath).stat().st_size
+
+    #Compare target size vs final size
+    print(f"-----------\nTarget size: {target_size} bytes")
+    print(f"Final size: {final_size} bytes\n-----------") 
 
 
 if __name__ == "__main__":
-    compressVid("VID_20241008_165811182.mp4","Discord_Bot\cmds\Library\Processed_Videos","VID_20241008_165811182",8)
+    compressVid("Change and Slam.mp4","Discord_Bot\cmds\Library\Processed_Videos","Change and Slam.mp4",9,True)
