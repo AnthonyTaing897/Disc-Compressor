9a8363e7ebd34d7ac88e28affadc049e71c72908
Loafcifer <taing897@gmail.com>
2025-12-03 20:49:40 +1100

    Updated new Disc Compress Decals + Improved compression function performance
    Due to a slight color change with the design. New icons and banners have been made.
    
    Along with this the Compress Function now will encode videos using h264 to improve speed.

--- Discord_Bot/cmds/func/Compress_Video_Function.py
+++ Discord_Bot/cmds/func/Compress_Video_Function.py
@@ -7,21 +7,21 @@ def compressVid (video_file:Path,processed_drct:Path,filename:Path, target_file_
     min_audio_bitrate = 32000
     max_audio_bitrate = 256000
 
-    #file size (in bytes)
+    # File size (in bytes)
     target_size = target_file_size * 1024 * 1024
     
     probed_vid = ffmpeg.probe(str(video_file))
 
-    #Video duration (secs)
+    # Video duration (secs)
     vid_duration = float(probed_vid['format']['duration'])
 
-    #Audio bitrate (bps)
+    # Audio bitrate (bps)
     audio_bitrate = float(next((s for s in probed_vid['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
 
-    #Calculate target video bitrate (bps) 
+    # Calculate target video bitrate (bps) 
     target_bitrate = ((target_size * 8) / (1.073741824 * vid_duration)) #1.073741824 to convert bytes to bits
 
-    #Calculate target audio bitrate (bps)
+    # Calculate target audio bitrate (bps)
     if 10 * audio_bitrate > target_bitrate:
         audio_bitrate = target_bitrate / 10
         if audio_bitrate < min_audio_bitrate < target_bitrate:
@@ -35,25 +35,27 @@ def compressVid (video_file:Path,processed_drct:Path,filename:Path, target_file_
     video = ffmpeg.input(video_file)
     
     compressed_video_filepath = f"{processed_drct}/{Path(filename).stem}_compressed.mp4"
-    # Output compressed video
+
 
     if compressAudio:
-        ffmpeg.output(video, compressed_video_filepath,
-                      vcodec = 'libx265',
-                      video_bitrate = int(video_bitrate),
-                      audio_bitrate = int(audio_bitrate),
-                      format='mp4').run(overwrite_output=True)
-                    
+        compression_params = ffmpeg.output(video, compressed_video_filepath,
+                                           vcodec = 'libx264',
+                                           video_bitrate = int(video_bitrate),
+                                           audio_bitrate = int(audio_bitrate),
+                                           format='mp4')
     else:
-        ffmpeg.output(video, compressed_video_filepath,
-                      vcodec = 'libx265',
-                      video_bitrate = int(video_bitrate),
-                      format='mp4').run(overwrite_output=True)
+        compression_params = ffmpeg.output(video, compressed_video_filepath,
+                                           vcodec = 'libx264',
+                                           video_bitrate = int(video_bitrate),
+                                           format='mp4')
+    
+    # Output compressed video
+    compression_params.run(overwrite_output=True)
 
-    #Get final file size
+    # Get final file size
     final_size = Path(compressed_video_filepath).stat().st_size
 
-    #Compare target size vs final size
+    # Compare target size vs final size
     print(f"-----------\nTarget size: {target_size} bytes")
     print(f"Final size: {final_size} bytes\n-----------") 
 
